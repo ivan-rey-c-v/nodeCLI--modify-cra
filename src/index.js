b5ecaf6a3d8ace63424fa4117cf32fcbf6a22d7b
ivan rey c.v <ivan.rey.c.v@gmail.com>
2019-05-10 19:03:20 +0800

    - Update package json - Use ESM - Add .prettierrc

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
-const {Command, flags} = require('@oclif/command')
+import { Command, flags } from '@oclif/command'
+import test from './test'
 
-class IrcvModifyCraCommand extends Command {
-  async run() {
-    const {flags} = this.parse(IrcvModifyCraCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from .\\src\\index.js`)
-  }
+// ESM works with this file
+test()
+
+class CLI extends Command {
+	async run() {
+		const { flags } = this.parse(CLI)
+		const name = flags.name || 'world'
+		this.log(`hello ${name} from .\\src\\index.js`)
+	}
 }
 
-IrcvModifyCraCommand.description = `Describe the command here
+CLI.description = `Describe the command here
 ...
 Extra documentation goes here
 `
 
-IrcvModifyCraCommand.flags = {
-  // add --version flag to show CLI version
-  version: flags.version({char: 'v'}),
-  // add --help flag to show CLI version
-  help: flags.help({char: 'h'}),
-  name: flags.string({char: 'n', description: 'name to print'}),
+CLI.flags = {
+	// add --version flag to show CLI version
+	version: flags.version({ char: 'v' }),
+	// add --help flag to show CLI version
+	help: flags.help({ char: 'h' }),
+	name: flags.string({ char: 'n', description: 'name to print' })
 }
 
-module.exports = IrcvModifyCraCommand
+// - it seems "export default CLI" does not work here (just this file)
+//   even if using the ESM
+// - import above works though
+// - other files have no issue
+module.exports = CLI
